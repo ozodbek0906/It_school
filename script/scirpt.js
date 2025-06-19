c7df0addce9fcaa8ec74727990bb0bcca6a826ce
ozodbek0906 <ozodbekshakarboyev@gmail.com>
2025-06-19 16:38:18 +0500

    It_school
    It_school

---
+++ script/scirpt.js
@@ -0,0 +1,8 @@
+let navLinks = document.querySelector("#navLinks");
+
+        const showMenu = () => {
+            navLinks.style.right = "0";
+        }
+        const hideMenu = () => {
+            navLinks.style.right = "-200px";
+        }
